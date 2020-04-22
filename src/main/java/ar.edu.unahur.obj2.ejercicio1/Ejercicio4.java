05a6e26eda61096d48ccd2deaaee8baa4b66062c
Laura Pereyra <lautiaguii@gmail.com>
2020-04-22 17:48:31 -0300

    ejercicio3

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio4.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio4.java
@@ -1,24 +1,32 @@
 package ar.edu.unahur.obj2.ejercicio1;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Ejercicio4 {
     public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+        int cantidadCompañeros;
+        cantidadCompañeros = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de compañeros"));
+        char[] nombreyApellido = new char [cantidadCompañeros];
+        List<Character> personasQueEmpiezanConEsaLetra= new ArrayList();
 
+        for (int e =0 ; e <cantidadCompañeros ;e++){
+            System.out.print((e+1)+ ". Digite los nombres de tus compañeros");
+            nombreyApellido[e] = entrada.next().charAt(0);
+        }
 
-        List nombreyApellido = new ArrayList<>();
-        nombreyApellido.add(JOptionPane.showInputDialog("Ingrese los nombres y apellidos:"));
 
         char letra;
         letra = JOptionPane.showInputDialog("Ingrese una letra:").charAt(0);
-        ArrayList personasQueEmpiezanConEsaLetra = new ArrayList<>();
 
 
-        for (int e =0 ; e <  nombreyApellido.size();e++){
-            if( nombreyApellido.get(e).charAt(0)== letra){
+        //nombreyApellido.forEach(e -> e.charAt(0) == letra);
+        for (int e =0 ; e < cantidadCompañeros ;e++){
+            if( nombreyApellido.get(e).charAt(0) == letra){
                 personasQueEmpiezanConEsaLetra.add(nombreyApellido.get(e));
             }
 
