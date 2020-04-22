4e5a0f2a30273921d38769050b949fa934ceabd0
Laura Pereyra <lautiaguii@gmail.com>
2020-04-22 18:47:43 -0300

    Ejercicio

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio4.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio4.java
@@ -1,6 +1,7 @@
 package ar.edu.unahur.obj2.ejercicio1;
 
 import javax.swing.JOptionPane;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,31 +9,24 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        Scanner entrada= new Scanner(System.in);
-        int cantidadCompañeros;
-        cantidadCompañeros = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de compañeros"));
-        char[] nombreyApellido = new char [cantidadCompañeros];
-        List<Character> personasQueEmpiezanConEsaLetra= new ArrayList();
-
-        for (int e =0 ; e <cantidadCompañeros ;e++){
-            System.out.print((e+1)+ ". Digite los nombres de tus compañeros");
-            nombreyApellido[e] = entrada.next().charAt(0);
-        }
+        List  nombresyApellidos = new ArrayList();
+        nombresyApellidos.add(JOptionPane.showInputDialog("Ingresar los nombres y apellido"));
 
 
         char letra;
         letra = JOptionPane.showInputDialog("Ingrese una letra:").charAt(0);
+        ArrayList personasQueEmpiezanConLaLetra = new ArrayList<>();
 
 
-        //nombreyApellido.forEach(e -> e.charAt(0) == letra);
-        for (int e =0 ; e < cantidadCompañeros ;e++){
+
+        for (int e =0 ; e < nombreyApellido.size() ;e++){
             if( nombreyApellido.get(e).charAt(0) == letra){
-                personasQueEmpiezanConEsaLetra.add(nombreyApellido.get(e));
+                personasQueEmpiezanConLaLetra.add(nombreyApellido.get(e));
             }
 
         }
 
-        System.out.print("Los nombres que empiezan con la letra ingresada son:" +"  " +personasQueEmpiezanConEsaLetra );
+        System.out.print("Los nombres que empiezan con la letra ingresada son:" +"  " +personasQueEmpiezanConEsaLetra.get() );
     }
 
 }
