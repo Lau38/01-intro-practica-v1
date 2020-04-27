9b272f8ba8f860ecd6e2a7342d4b6ab82146e52c
Laura Pereyra <lautiaguii@gmail.com>
2020-04-26 22:00:00 -0300

    terminado

---
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio7.java
@@ -0,0 +1,38 @@
+package ar.edu.unahur.obj2.ejercicio1;
+
+import javax.swing.*;
+import java.util.Random;
+
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+
+        Random r = new Random();
+        int matriz[] [], nroFilas,nroColumnas;
+
+        nroFilas= Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero de filas:"));
+        nroColumnas=Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero de columnas:"));
+
+        matriz = new int [nroFilas] [nroColumnas];
+
+        System.out.println("Digite la matriz:  ");
+        for (int i=0; i<nroFilas ; i++) {
+            for (int j=0; j<nroColumnas; j++) {
+                System.out.print("Matriz ["+ i +"] ["+ j +"]: ");
+                matriz[i] [j] = r.nextInt(100);
+            }
+
+        }
+
+        System.out.println("\nLa matriz es :");
+        for (int i=0; i < nroFilas ; i++){
+            for (int j=0 ; j < nroColumnas ;j++){
+                System.out.print(matriz[i] [j] + "\t");
+            }
+            System.out.println("");
+        }
+
+
+    }
+}
+
